9c8a0d5e6b922b2e0d5b3e82993b805abec27c85
Artur <artur.backetov@yandex.ru>
2023-03-04 10:41:16 +0300

    Add new homework

---
+++ JavaHomeWork.java
@@ -0,0 +1,17 @@
+public class JavaHomeWork {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+            System.out.print("Input N: ");
+            int N = input.nextInt();
+            System.out.println("Result: " + calculate(N));
+        }
+
+        public static double calculate(int N){
+            double result = 0;
+            for (int i = 1, j = N; i <= N && j >= 1; i++, j--) {
+                result += Math.pow(i, j);
+            }
+            return result;
+        }
+    }
+}
